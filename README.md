f5a3c228186bed34d8dce7ecfe65f9e435433a28
Jon Campbell <joncamp@outlook.com>
2023-11-07 15:03:33 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-usb-example-electron-forge
+
+Electron-forge example for node-usb, based on https://github.com/node-usb/node-usb-example-electron
+
+Built using https://www.electronforge.io/templates/typescript-+-webpack-template, with changes to add node-usb and the example code from above.
---
+++ src/preload.ts
@@ -0,0 +1,19 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { ipcRenderer } from 'electron'
+
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector: string, text: string) => {
+        const element = document.getElementById(selector)
+        if (element) element.innerText = text
+    }
+
+    for (const dependency of ['chrome', 'node', 'electron']) {
+        replaceText(`${dependency}-version`, process.versions[dependency])
+    }
+
+    ipcRenderer.on('devices', (_event, text) => replaceText('devices', text))
+})
